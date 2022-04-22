032c464ba061240174341e0a3899c2e35464ffd6
Tamrin <101350202+tamrin01@users.noreply.github.com>
2022-04-22 16:30:57 +0800

    Add files via upload

---
+++ class_variabel.py
@@ -0,0 +1,22 @@
+
+#proses memasukan data ke dalam variabel
+nama = "Tamrin"
+print(nama)
+
+#nilai dan tipe data dalam variabel  dapat diubah
+umur = 21               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "dua puluh satu" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Tamrin"
+namaBelakang = ".."
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "olahraga"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+
+
